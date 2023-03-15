e8c7c2da918000f08bbd48ad9c74cad4bdfed570
claytonfortunato <claytonreis10@gmail.com>
2023-03-14 23:29:17 -0300

    feat: add img no component

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,11 @@
+import ProductImg from "./components/ProductImg";
 import ProductInfo from "./components/ProductInfo";
 import * as C from "./styles";
 
 function App() {
   return (
     <C.Container>
+      <ProductImg />
       <ProductInfo />
     </C.Container>
   );
---
+++ src/components/ProductImg/index.tsx
@@ -0,0 +1,13 @@
+import SofaImg from "../../assets/sofa.jpg";
+
+import * as C from "./styles";
+
+const ProductImg = () => {
+  return (
+    <C.Container>
+      <img src={SofaImg} />
+    </C.Container>
+  );
+};
+
+export default ProductImg;
