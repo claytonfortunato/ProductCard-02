650c2ef93030bb4f3477a062da35b25e2ebff4a2
claytonfortunato <claytonreis10@gmail.com>
2023-03-16 02:45:53 -0300

    feat: mudança  de icon

--- src/components/ProductImg/index.tsx
+++ src/components/ProductImg/index.tsx
@@ -1,9 +1,8 @@
 import { useState } from "react";
 import SofaImg from "../../assets/sofa.jpg";
 import SofaGif from "../../assets/sofa.gif";
-
-import { TiArrowSync } from "react-icons/Ti";
-import { AiOutlineClose } from "react-icons/ai";
+import Arrow from "../../assets/360icon.svg";
+import Close from "../../assets/closeicon.svg";
 
 import * as C from "./styles";
 
@@ -12,16 +11,12 @@ export const ProductImg = () => {
 
   return toggle ? (
     <C.Container>
-      <AiOutlineClose
-        size={32}
-        color="#271A45"
-        onClick={() => setToggle(!toggle)}
-      />
+      <img src={Close} className="Close" onClick={() => setToggle(!toggle)} />
       <img src={SofaGif} />
     </C.Container>
   ) : (
     <C.Container>
-      <TiArrowSync size={32} onClick={() => setToggle(!toggle)} />
+      <img src={Arrow} className="Arrow" onClick={() => setToggle(!toggle)} />
       <img src={SofaImg} />
     </C.Container>
   );
--- src/components/ProductImg/styles.ts
+++ src/components/ProductImg/styles.ts
@@ -3,16 +3,34 @@ import styled from "styled-components";
 export const Container = styled.div`
   position: relative;
 
-  img {
-    width: 449px;
-    height: 222px;
-    object-fit: cover;
-  }
+  .Close {
+    width: 20px;
+    height: 20px;
 
-  svg {
     position: absolute;
     top: 0;
     right: 0;
     cursor: pointer;
   }
+
+  .Arrow {
+    width: 45px;
+    height: 30px;
+
+    position: absolute;
+    top: 0;
+    right: 0;
+    cursor: pointer;
+  }
+
+  img {
+    width: 449px;
+    height: 222px;
+    object-fit: cover;
+  }
+`;
+
+export const Image = styled.img`
+  width: 30px;
+  height: 30px;
 `;
